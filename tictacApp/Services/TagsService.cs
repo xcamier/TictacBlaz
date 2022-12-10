3931556343c15bb0b87a47d87223964205460ba5
Xavier CAMIER <camierxcom@icloud.com>
2022-12-10 20:12:49 +0100

    CRUD for the tags

--- tictacApp/Services/TagsService.cs
+++ tictacApp/Services/TagsService.cs
@@ -13,10 +13,54 @@ public class TagsService
         _dbFactory = dbFactory;
     }
 
+    public TictacDBContext? GetDBContext()
+    {
+        return _dbFactory.CreateDbContext();
+    }
+
     public async Task<Tag[]> GetTagsAsync()
     {
         using var context = _dbFactory.CreateDbContext();
         
         return await context.Tags.OrderBy(t => t.Label).ToArrayAsync();
+    }
+
+    public async Task<Tag?> FindTagFromIdAsync(TictacDBContext? dbContext, int tagId)
+    {    
+        if (dbContext is not null && dbContext.Tags is not null)
+        {
+            return await dbContext.Tags.SingleOrDefaultAsync(c => c.Id == tagId); 
+        }
+
+        return null;
+    }
+
+    public async Task<bool> AddTagAsync(Tag? tagToAdd)
+    {
+        if (tagToAdd != null)
+        {
+            using var context = _dbFactory.CreateDbContext();
+            
+            context.Tags?.Add(tagToAdd);
+
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> DeleteTagAsync(Tag tagToDelete)
+    {
+        if (tagToDelete != null)
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            context.Remove(tagToDelete);
+            
+            return await context.SaveChangesAsync() > 0;
+
+        }
+
+        return false;
     }
 }
