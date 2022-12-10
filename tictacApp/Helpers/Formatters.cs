a216a6111f8e09cf761c1dd7f924d1dfa6ac68ce
Xavier CAMIER <camierxcom@icloud.com>
2022-12-10 17:54:05 +0100

    Timelogs page shows total time spent and better navigation

---
+++ tictacApp/Helpers/Formatters.cs
@@ -0,0 +1,35 @@
+namespace tictacApp.Formatters;
+
+public class FormattersHelper
+{
+    public static string MinToString(int minutes)
+    {
+        TimeSpan ts = TimeSpan.FromMinutes(minutes);
+
+        string res = string.Empty;
+        string fmt = "00";
+        
+        if (ts.Days > 0)
+        {
+            res = $"{ts.Days.ToString(fmt)}d";
+        }
+
+        if (ts.Hours > 0)
+        {
+            if (res != string.Empty)
+            {
+                res = string.Concat(res, " ");
+            }
+
+            res = string.Concat(res, ts.Hours.ToString(fmt), "h");
+        }
+
+        if (res != string.Empty)
+        {
+            res = string.Concat(res, " ");
+        }
+        res = string.Concat(res, ts.Minutes.ToString(fmt), "min");
+
+        return res;
+    }
+}
--- tictacApp/Services/TimeLogsService.cs
+++ tictacApp/Services/TimeLogsService.cs
@@ -22,6 +22,18 @@ public class TimeLogsService
     {
         using var context = _dbFactory.CreateDbContext();
         
+        DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day);
+        DateTime endDate = startDate.AddDays(1);
+
+        return await context.TimeLogs.
+                        Where(t => t.StartDate >= startDate && t.StartDate < endDate).
+                        ToArrayAsync();
+    }
+
+    public async Task<int> GetTimeSpentInWeek(DateOnly currentDate)
+    {
+        using var context = _dbFactory.CreateDbContext();
+        
         DateTime dt = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day);
 
         bool isSunday = dt.DayOfWeek == 0;
@@ -31,8 +43,7 @@ public class TimeLogsService
 
         return await context.TimeLogs.
                         Where(t => t.StartDate >= startDate && t.StartDate < endDate).
-                        OrderByDescending(t => startDate).
-                        ToArrayAsync();
+                        SumAsync(s => s.TimeSpentInMin);
     }
 
     public async Task<TimeLog?> FindTimeLogFromIdAsync(TictacDBContext? dbContext, int timeLogId)
