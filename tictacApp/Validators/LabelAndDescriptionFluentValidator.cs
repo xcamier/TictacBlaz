710805e094d076d4e4d8be96c5e59459c5413f11
Xavier CAMIER <camierxcom@icloud.com>
2022-12-29 20:58:48 +0100

    Improvements in the fluent validation

--- tictacApp/Validators/LabelAndDescriptionFluentValidator.cs
+++ tictacApp/Validators/LabelAndDescriptionFluentValidator.cs
@@ -4,27 +4,26 @@ using tictacApp.Interfaces;
 
 namespace tictacApp.Validators;
 
-    public class LabelAndDescriptionFluentValidator<T> : AbstractValidator<T> where T: class, IIdLabel, IDescription
+public class LabelAndDescriptionFluentValidator<T> : AbstractValidator<T> where T: class, IIdLabel, IDescription
+{
+    private int _labelMin = Constants.LabelMinLength;
+    private int _labelMax = Constants.LabelShortLength;
+    private int _descriptionMax = Constants.DescriptionStandardLength;
+
+    public LabelAndDescriptionFluentValidator(int labelMin, int labelMax, int descriptionMax)
     {
-        private int _labelMin = Constants.LabelMinLength;
-        private int _labelMax = Constants.LabelShortLength;
-        private int _descriptionMax = Constants.DescriptionStandardLength;
+        RuleFor(p => p.Label).
+                        NotEmpty().WithMessage("Please specify a label").
+                        Length(labelMin, labelMax);
 
-        public LabelAndDescriptionFluentValidator(int labelMin, int labelMax, int descriptionMax)
-        {
-            RuleFor(p => p.Label).
-                            NotNull().
-                            MaximumLength(labelMax).
-                            MinimumLength(labelMin);
-
-            RuleFor(p =>p.Description).MaximumLength( descriptionMax);
-        }
-
-        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-        {
-            var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
-            if (result.IsValid)
-                return Array.Empty<string>();
-            return result.Errors.Select(e => e.ErrorMessage);
-        };
+        RuleFor(p =>p.Description).MaximumLength( descriptionMax);
     }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
--- tictacApp/Validators/LabelFluentValidator.cs
+++ tictacApp/Validators/LabelFluentValidator.cs
@@ -4,24 +4,23 @@ using tictacApp.Interfaces;
 
 namespace tictacApp.Validators;
 
-    public class LabelFluentValidator<T> : AbstractValidator<T> where T: class, IIdLabel
+public class LabelFluentValidator<T> : AbstractValidator<T> where T: class, IIdLabel
+{
+    private int _labelMin = Constants.Label1CharLength;
+    private int _labelMax = Constants.LabelShortLength;
+
+    public LabelFluentValidator(int labelMin, int labelMax)
     {
-        private int _labelMin = Constants.Label1CharLength;
-        private int _labelMax = Constants.LabelShortLength;
-
-        public LabelFluentValidator(int labelMin, int labelMax)
-        {
-            RuleFor(p => p.Label).
-                                NotNull().
-                                MaximumLength(labelMax).
-                                MinimumLength(labelMin);
-        }
-
-        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-        {
-            var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
-            if (result.IsValid)
-                return Array.Empty<string>();
-            return result.Errors.Select(e => e.ErrorMessage);
-        };
+        RuleFor(p => p.Label).
+                        NotEmpty().WithMessage("Please specify a label").
+                        Length(labelMin, labelMax);
     }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
