902442024d587266c7c244c893c85d7d14514c99
Xavier CAMIER <camierxcom@icloud.com>
2023-01-11 20:21:18 +0100

    issue-22: [Observations] Possibility to weight an observation

---
+++ tictacApp/Migrations/20230111191851_Observations_Rating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tictacApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ObservationsRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SelectedRating",
+                table: "Observations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectedRating",
+                table: "Observations");
+        }
+    }
+}
