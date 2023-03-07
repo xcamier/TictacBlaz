b73a1077b10f423b28b80076779861f1fb3049b7
Xavier CAMIER <camierxcom@icloud.com>
2023-03-07 18:51:55 +0100

    issue-26: [Timelogs] The total for the week shall be displayed in hours

--- tictacApp/Helpers/Formatters.cs
+++ tictacApp/Helpers/Formatters.cs
@@ -4,37 +4,6 @@ namespace tictacApp.Helpers;
 
 public class FormattersHelper
 {
-    public static string MinToString(int minutes)
-    {
-        TimeSpan ts = TimeSpan.FromMinutes(minutes);
-
-        string res = string.Empty;
-        string fmt = "00";
-        
-        if (ts.Days > 0)
-        {
-            res = $"{ts.Days.ToString(fmt)}d";
-        }
-
-        if (ts.Hours > 0)
-        {
-            if (res != string.Empty)
-            {
-                res = string.Concat(res, " ");
-            }
-
-            res = string.Concat(res, ts.Hours.ToString(fmt), "h");
-        }
-
-        if (res != string.Empty)
-        {
-            res = string.Concat(res, " ");
-        }
-        res = string.Concat(res, ts.Minutes.ToString(fmt), "min");
-
-        return res;
-    }
-
     public static string MinToHoursAndMinAsString(int minutes)
     {
         TimeSpan ts = TimeSpan.FromMinutes(minutes);
